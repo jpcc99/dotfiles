a8fafb3453d6000457d44f1024273037e3a118e9
João Pedro da Costa <59283595+jpcc99@users.noreply.github.com>
2020-02-25 00:45:22 -0300

    Update pomodoro.zsh

--- .scripts/pomodoro.zsh
+++ .scripts/pomodoro.zsh
@@ -1,9 +1,13 @@
 #!/bin/zsh
-# A minimalistic pomodoro using systemd timers
-#sleep 1500 && zenity --warning --text="25 minutes passed" 
+#	@author:	João Pedro da Costa
+#	@email:		jpcc.dev@gmail.com
+#	@git-repo:	github.com/jpcc99
+#	Description:
+#       A minimalistic pomodoro script
+
 notify-send '🍅 Pomodoro' 'Stay focused! 🎯'
 sleep 1500 &&  notify-send '🍅 Pomodoro' '25 minutes passed' \
     && paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
-#sleep 300 && zenity --warning --text="Back to workd" # Short pause
+# Short pause
 sleep 300 && notify-send '🍅 Pomodoro' 'Back to work!' \
     && paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
